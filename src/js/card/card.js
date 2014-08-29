b764ec74db30de85b0e4fa76e490b1e7c2c980a3
Kent Andersen <kentareandersen@gmail.com>
2014-08-29 13:44:21 +0200

    la til defaults og url til kort endepunkt

--- src/js/card/card.js
+++ src/js/card/card.js
@@ -2,6 +2,13 @@ var BaseModel = require("../base/model");
 
 var CardModel = BaseModel.extend({
 
+  idAttribute: "key",
+
+  defaults: {
+    "title": "",
+    "key": "",
+    "detailsUrl": ""
+  }
 });
 
 module.exports = CardModel;
--- src/js/card/cards.js
+++ src/js/card/cards.js
@@ -2,7 +2,11 @@ var BaseCollection = require("../base/collection");
 var CardModel = require("./card");
 
 var CardCollection = BaseCollection.extend({
-    model: CardModel
+
+  model: CardModel,
+
+  url: "rest/kort"
+
 });
 
 module.exports = CardCollection;
