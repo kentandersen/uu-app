1d58bd8bbc895f161d986a71291a2c521c1c6a99
Kent Andersen <kentareandersen@gmail.com>
2014-11-15 23:16:29 +0100

    bytta til å bruke vinyl transform istede for gulp-browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,32 @@
 var path = require('path');
 var gulp = require('gulp');
+var transform = require('vinyl-transform');
 var sourcemaps = require('gulp-sourcemaps');
 
 var rename = require("gulp-rename");
 var rimraf = require('rimraf');
 
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
+
 var hbsfy = require('hbsfy');
 
 var source = require('vinyl-source-stream');
 var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 
-var less = require('gulp-less-sourcemap');
+var less = require('gulp-less');
+
+var isWatch = false;
+
+function swallowError (error) {
+  if (isWatch) {
+    this.emit('end');
+    console.log(error.toString());
+  } else {
+    this.removeListener('error', swallowError);
+    this.emit('error', error);
+  }
+}
 
 // Paths
 var build  = path.join(__dirname, 'build');
@@ -37,8 +51,9 @@ gulp.task('less', function() {
   return gulp.src(webapp + '/main.less')
     .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(sourcemaps.write())
+    .on('error', swallowError)
     .pipe(rename("styles.css"))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(build));
 });
 
@@ -52,10 +67,17 @@ gulp.task('js', function() {
     extensions: ["hb"]
   });
   return gulp.src(webapp + '/main.js')
-    .pipe(browserify({
-      transform: [handlebars],
-      debug: true
+    .pipe(transform(function(filename) {
+      return browserify(filename, {
+        transform: [handlebars],
+        debug: true
+      })
+      .ignore("jquery")
+      .ignore("underscore")
+      .require([{expose: "backbone", file: path.resolve("./shim/backbone.js")}])
+      .bundle();
     }))
+    .on('error', swallowError)
     .pipe(gulp.dest(build))
 });
 
@@ -65,6 +87,7 @@ gulp.task('clean', function(done) {
 });
 
 gulp.task('dev', function() {
+  isWatch = true;
   gulp.watch(webapp + '/**/*.less', ['less']);
   gulp.watch(webapp + '/**/*.{js,hb}', ['js']);
   gulp.watch(webapp + '/index.html', ['html']);
